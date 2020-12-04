c2e40100f64209a860ef4d174cbe90934cc6df16
Daisuke (yet another) Maki <maki.daisuke+gh@gmail.com>
2020-12-04 13:07:24 +0900

    Use panic to return matching result
    But, it was slower than normal return :-(

--- match.go
+++ match.go
@@ -1,113 +1,121 @@
 package reaot
 
-func Match(re Regexp, s string) bool {
-	for i := 0; i < len(s); i++ {
-		if re.match(matchContext{nil, 0, i, []rune(s)}, i, func(c matchContext, _ int) *matchContext { return &c }) != nil {
-			return true
+func Match(re Regexp, s string) (ret bool) {
+	defer func() {
+		e := recover()
+		if e == nil {
+			return
 		}
+		if _, ok := e.(*matchContext); ok {
+			ret = true
+			return
+		}
+		panic(e) // rethrow error
+	}()
+	for i := 0; i < len(s); i++ {
+		re.match(matchContext{nil, 0, i, []rune(s)}, i, func(c matchContext, _ int) { panic(&c) })
+		// If you are here, matching failed. So, try next.
 	}
 	return false
 }
 
-func (re ReLit) match(c matchContext, p int, k func(matchContext, int) *matchContext) *matchContext {
+func (re ReLit) match(c matchContext, p int, k func(matchContext, int)) {
 	if len(c.str)-p < len(re) {
-		return nil
+		return
 	}
 	for i := 0; i < len(re); i++ {
 		if c.str[p+i] != re[i] {
-			return nil
+			return
 		}
 	}
-	return k(c, p+len(re))
+	k(c, p+len(re))
 }
 
-func (re ReNotNewline) match(c matchContext, p int, k func(matchContext, int) *matchContext) *matchContext {
+func (re ReNotNewline) match(c matchContext, p int, k func(matchContext, int)) {
 	if len(c.str) <= p || c.str[0] == '\n' {
-		return nil
+		return
 	}
-	return k(c, p+1)
+	k(c, p+1)
 }
 
-func (r *ReSeq) match(c matchContext, p int, k func(matchContext, int) *matchContext) *matchContext {
+func (r *ReSeq) match(c matchContext, p int, k func(matchContext, int)) {
 	if len(r.seq) == 0 {
-		return k(c, p)
+		k(c, p)
+		return
 	}
-	return r.seq[0].match(c, p, func(c matchContext, p1 int) *matchContext {
-		return (&ReSeq{r.seq[1:]}).match(c, p1, k)
+	r.seq[0].match(c, p, func(c matchContext, p1 int) {
+		(&ReSeq{r.seq[1:]}).match(c, p1, k)
 	})
 }
 
-func (r *ReAlt) match(c matchContext, p int, k func(matchContext, int) *matchContext) *matchContext {
+func (r *ReAlt) match(c matchContext, p int, k func(matchContext, int)) {
 	for _, re := range r.opts {
-		if c1 := re.match(c, p, k); c1 != nil {
-			return c1
-		}
+		re.match(c, p, k)
 	}
-	return nil
 }
 
-func (r *ReRepeat) match(c matchContext, p int, k func(matchContext, int) *matchContext) *matchContext {
+func (r *ReRepeat) match(c matchContext, p int, k func(matchContext, int)) {
 	re := r.re
 	prev := -1 // initial value must be a number which never equal to any position (i.e. positive integer)
-	var loop func(count int) func(matchContext, int) *matchContext
-	loop = func(count int) func(matchContext, int) *matchContext {
-		return func(c matchContext, p int) *matchContext {
+	var loop func(count int) func(matchContext, int)
+	loop = func(count int) func(matchContext, int) {
+		return func(c matchContext, p int) {
 			if prev == p { // Matched zero-length assertion. So, move ahead the next pattern.
-				return k(c, p)
+				k(c, p)
+				return
 			}
 			prev = p
 			if count < r.min {
-				return re.match(c, p, loop(count+1))
+				re.match(c, p, loop(count+1))
+				return
 			}
 			if count == r.max {
-				return k(c, p)
+				k(c, p)
+				return
 			}
-			c1 := re.match(c, p, loop(count+1))
-			if c1 != nil {
-				return c1
-			}
-			return k(c, p)
+			re.match(c, p, loop(count+1))
+			k(c, p)
 		}
 	}
-	return loop(0)(c, p)
+	loop(0)(c, p)
 }
 
-func (r *ReCap) match(c matchContext, p int, k func(matchContext, int) *matchContext) *matchContext {
-	return r.re.match(c.with(r.index, p), p, func(c matchContext, p1 int) *matchContext {
-		return k((&c).with(r.index, p1), p1)
+func (r *ReCap) match(c matchContext, p int, k func(matchContext, int)) {
+	r.re.match(c.with(r.index, p), p, func(c matchContext, p1 int) {
+		k((&c).with(r.index, p1), p1)
 	})
 }
 
-func (r ReBackRef) match(c matchContext, p int, k func(matchContext, int) *matchContext) *matchContext {
+func (r ReBackRef) match(c matchContext, p int, k func(matchContext, int)) {
 	cap := (&c).GetCaptured(uint(r))
 	if cap == nil {
 		// This means that the specified capture groups haven't matched any substring.
 		// It always fails (according to Perl regex).
-		return nil
+		return
 	}
-	return ReLit(cap).match(c, p, k)
+	ReLit(cap).match(c, p, k)
 }
 
-func (re ReAssertBegin) match(c matchContext, p int, k func(matchContext, int) *matchContext) *matchContext {
+func (re ReAssertBegin) match(c matchContext, p int, k func(matchContext, int)) {
 	if p != 0 {
-		return nil
+		return
 	}
-	return k(c, p)
+	k(c, p)
 }
 
-func (re ReAssertEnd) match(c matchContext, p int, k func(matchContext, int) *matchContext) *matchContext {
+func (re ReAssertEnd) match(c matchContext, p int, k func(matchContext, int)) {
 	if p != len(c.str) {
-		return nil
+		return
 	}
-	return k(c, p)
+	k(c, p)
 }
 
-func (re ReCharClass) match(c matchContext, p int, k func(matchContext, int) *matchContext) *matchContext {
+func (re ReCharClass) match(c matchContext, p int, k func(matchContext, int)) {
 	if len(c.str) < p+1 {
-		return nil
+		return
 	}
 	if !re.Contains(c.str[p]) {
-		return nil
+		return
 	}
-	return k(c, p+1)
+	k(c, p+1)
 }
--- type.go
+++ type.go
@@ -3,5 +3,5 @@ package reaot
 type Regexp interface {
 	//Compile()
 	String() string
-	match(matchContext, int, func(matchContext, int) *matchContext) *matchContext
+	match(matchContext, int, func(matchContext, int)) // This throws *matchContext via panic when it successfully match.
 }
