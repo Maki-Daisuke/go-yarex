464aad6983ecddb61ca03b257b84b16a4e79b2b8
Daisuke (yet another) Maki <maki.daisuke+gh@gmail.com>
2020-12-08 21:09:00 +0900

    Try user-stack instead of call-stack, but it's way slower than call-stack.
    Because, user-stack must be allocated in heap probably.

--- optreematch.go
+++ optreematch.go
@@ -1,15 +1,12 @@
 package reaot
 
 import (
-	"strings"
 	"unicode/utf8"
-	"unsafe"
 )
 
 func MatchOpTree(op OpTree, s string) bool {
-	ome := opMatchEngine{func(_ *opMatchContext) {}}
-	ctx := &opMatchContext{nil, s, "c0", 0}
-	if ome.exec(op, uintptr(unsafe.Pointer(ctx)), 0) != nil {
+	ome := newOpMatchEngine(func(_ *opMatchEngine) {})
+	if ome.exec(op, s, 0) {
 		return true
 	}
 	if _, ok := op.(*OpAssertBegin); ok {
@@ -17,103 +14,156 @@ func MatchOpTree(op OpTree, s string) bool {
 	}
 	minReq := op.minimumReq()
 	for i := 1; minReq <= len(s)-i; i++ {
-		ctx = &opMatchContext{nil, s, "c0", i}
-		if ome.exec(op, uintptr(unsafe.Pointer(ctx)), i) != nil {
+		if ome.exec(op, s, i) {
 			return true
 		}
 	}
 	return false
 }
 
-type opMatchEngine struct {
-	onSuccess func(*opMatchContext)
+type opStackFrame struct {
+	next OpTree
+	pos  int
+	key  string
+	val  int
 }
 
-func (ome opMatchEngine) exec(next OpTree, c uintptr, p int) *opMatchContext {
-	ctx := (*opMatchContext)(unsafe.Pointer(c))
-	str := ctx.str
+type opMatchEngine struct {
+	onSuccess func(*opMatchEngine)
+	stack     []opStackFrame
+	stacktop  int
+}
+
+func newOpMatchEngine(onSuccess func(*opMatchEngine)) *opMatchEngine {
+	return &opMatchEngine{
+		onSuccess: onSuccess,
+		stack:     make([]opStackFrame, 512),
+		stacktop:  0,
+	}
+}
+
+func (ome *opMatchEngine) push(f opStackFrame) {
+	if ome.stacktop >= len(ome.stack) {
+		ome.stack = append(ome.stack, make([]opStackFrame, len(ome.stack))...)
+	}
+	ome.stack[ome.stacktop] = f
+	ome.stacktop++
+}
+
+func (ome *opMatchEngine) pop() (OpTree, int) {
+	for {
+		if ome.stacktop == 0 {
+			return nil, 0
+		}
+		ome.stacktop--
+		f := ome.stack[ome.stacktop]
+		if f.next != nil {
+			return f.next, f.pos
+		}
+	}
+	panic("SHOULD NOT REACH HERE")
+}
+
+func (ome *opMatchEngine) findVal(k string) int {
+	for i := ome.stacktop - 1; i >= 0; i-- {
+		if ome.stack[i].key == k {
+			return ome.stack[i].val
+		}
+	}
+	return -1
+}
+
+func (ome opMatchEngine) exec(next OpTree, str string, p int) bool {
+LOOP:
 	for {
 		switch op := next.(type) {
 		case OpSuccess:
-			c := ctx.with("c0", p)
-			ome.onSuccess(&c)
-			return &c
+			return true
 		case *OpStr:
 			if len(str)-p < op.minReq {
-				return nil
+				next, p = ome.pop()
+				continue LOOP
 			}
 			for i := 0; i < len(op.str); i++ {
 				if str[p+i] != op.str[i] {
-					return nil
+					next, p = ome.pop()
+					continue LOOP
 				}
 			}
 			next = op.follower
 			p += len(op.str)
 		case *OpAlt:
-			if r := ome.exec(op.follower, c, p); r != nil {
-				return r
-			}
-			next = op.alt
+			ome.push(opStackFrame{op.alt, p, "", 0})
+			next = op.follower
 		case *OpRepeat:
-			prev := ctx.findVal(op.key)
+			prev := ome.findVal(op.key)
 			if prev == p { // This means zero-width matching occurs.
 				next = op.alt // So, terminate repeating.
-				continue
+				continue LOOP
 			}
-			ctx2 := ctx.with(op.key, p)
-			if r := ome.exec(op.follower, uintptr(unsafe.Pointer(&ctx2)), p); r != nil {
-				return r
-			}
-			next = op.alt
+			ome.push(opStackFrame{op.alt, p, op.key, p})
+			next = op.follower
 		case *OpClass:
 			if len(str)-p < op.minReq {
-				return nil
+				next, p = ome.pop()
+				continue LOOP
 			}
 			r, size := utf8.DecodeRuneInString(str[p:])
 			if size == 0 || r == utf8.RuneError {
-				return nil
+				next, p = ome.pop()
+				continue LOOP
 			}
 			if !op.cls.Contains(r) {
-				return nil
+				next, p = ome.pop()
+				continue LOOP
 			}
 			next = op.follower
 			p += size
 		case *OpNotNewLine:
 			if len(str)-p < op.minReq {
-				return nil
+				next, p = ome.pop()
+				continue LOOP
 			}
 			r, size := utf8.DecodeRuneInString(str[p:])
 			if size == 0 || r == utf8.RuneError {
-				return nil
+				next, p = ome.pop()
+				continue LOOP
 			}
 			if r == '\n' {
-				return nil
+				next, p = ome.pop()
+				continue LOOP
 			}
 			next = op.follower
 			p += size
 		case *OpCaptureStart:
-			ctx2 := ctx.with(op.key, p)
-			return ome.exec(op.follower, uintptr(unsafe.Pointer(&ctx2)), p)
-		case *OpCaptureEnd:
-			ctx2 := ctx.with(op.key, p)
-			return ome.exec(op.follower, uintptr(unsafe.Pointer(&ctx2)), p)
-		case *OpBackRef:
-			s, ok := ctx.GetCaptured(op.key)
-			if !ok || !strings.HasPrefix(str[p:], s) {
-				return nil
-			}
+			ome.push(opStackFrame{nil, 0, op.key, p})
 			next = op.follower
-			p += len(s)
+		case *OpCaptureEnd:
+			ome.push(opStackFrame{nil, 0, op.key, p})
+			next = op.follower
+		case *OpBackRef:
+			//s, ok := ctx.GetCaptured(op.key)
+			//if !ok || !strings.HasPrefix(str[p:], s) {
+			//	return nil
+			//}
+			//next = op.follower
+			//p += len(s)
+			// Not imlemented yet
+			next, p = ome.pop()
 		case *OpAssertBegin:
 			if p != 0 {
-				return nil
+				next, p = ome.pop()
+				continue LOOP
 			}
 			next = op.follower
 		case *OpAssertEnd:
 			if p != len(str) {
-				return nil
+				next, p = ome.pop()
+				continue LOOP
 			}
 			next = op.follower
+		default:
+			return false
 		}
 	}
 }
