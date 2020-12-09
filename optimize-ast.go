7aafc30031ce5e0f7151f270d8f4039250325f8c
Daisuke (yet another) Maki <maki.daisuke+gh@gmail.com>
2020-12-09 13:53:39 +0900

    Optimize Ast by flattening Seqs and Alts

--- optimize-ast.go
+++ optimize-ast.go
@@ -1,8 +1,51 @@
 package reaot
 
+import "fmt"
+
 func optimizeAst(re Ast) Ast {
+	re = optimizeAstFlattenSeqAndAlt(re)
 	re = optimizeAstUnwrapSingletonSeqAndAlt(re)
 	return re
+}
+
+// optimizeAstFlattenSeqAndAlt flattens nested AstSeqs and AstAlts
+func optimizeAstFlattenSeqAndAlt(re Ast) Ast {
+	switch v := re.(type) {
+	case *AstSeq:
+		out := make([]Ast, 0, len(v.seq))
+		for _, r := range v.seq {
+			r = optimizeAstFlattenSeqAndAlt(r)
+			if as, ok := r.(*AstSeq); ok {
+				out = append(out, as.seq...)
+			} else {
+				out = append(out, r)
+			}
+		}
+		return &AstSeq{out}
+	case *AstAlt:
+		out := make([]Ast, 0, len(v.opts))
+		for _, r := range v.opts {
+			r = optimizeAstFlattenSeqAndAlt(r)
+			if aa, ok := r.(*AstAlt); ok {
+				out = append(out, aa.opts...)
+			} else {
+				out = append(out, r)
+			}
+		}
+		return &AstAlt{out}
+	case *AstRepeat:
+		out := *v
+		out.re = optimizeAstFlattenSeqAndAlt(v.re)
+		return &out
+	case *AstCap:
+		out := *v
+		out.re = optimizeAstFlattenSeqAndAlt(v.re)
+		return &out
+	case AstLit, AstNotNewline, AstAssertBegin, AstAssertEnd, AstBackRef, AstCharClass:
+		return v
+	default:
+		panic(fmt.Errorf("IMPLEMENT optimizeAstFlattenSeqAndAlt for %T", re))
+	}
 }
 
 // optimizeAstUnwrapSingletonSeqAndAlt joins adjacent literals, and unwrap seqs and alts
