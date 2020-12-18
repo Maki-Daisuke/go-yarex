3ff63b9f3f2af8eec4ca36c2b0179262d9570668
Daisuke (yet another) Maki <maki.daisuke+gh@gmail.com>
2020-12-18 16:43:39 +0900

    Use bool as return value of exec instead of *opMatchContext

--- optreematch.go
+++ optreematch.go
@@ -9,7 +9,7 @@ import (
 func matchOpTree(op OpTree, s string) bool {
 	ome := opMatchEngine{func(_ *opMatchContext) {}}
 	ctx := &opMatchContext{nil, s, contextKey{'c', 0}, 0}
-	if ome.exec(op, uintptr(unsafe.Pointer(ctx)), 0) != nil {
+	if ome.exec(op, uintptr(unsafe.Pointer(ctx)), 0) {
 		return true
 	}
 	if _, ok := op.(*OpAssertBegin); ok {
@@ -18,7 +18,7 @@ func matchOpTree(op OpTree, s string) bool {
 	minReq := op.minimumReq()
 	for i := 1; minReq <= len(s)-i; i++ {
 		ctx = &opMatchContext{nil, s, contextKey{'c', 0}, i}
-		if ome.exec(op, uintptr(unsafe.Pointer(ctx)), i) != nil {
+		if ome.exec(op, uintptr(unsafe.Pointer(ctx)), i) {
 			return true
 		}
 	}
@@ -29,7 +29,7 @@ type opMatchEngine struct {
 	onSuccess func(*opMatchContext)
 }
 
-func (ome opMatchEngine) exec(next OpTree, c uintptr, p int) *opMatchContext {
+func (ome opMatchEngine) exec(next OpTree, c uintptr, p int) bool {
 	ctx := (*opMatchContext)(unsafe.Pointer(c))
 	str := ctx.str
 	for {
@@ -37,21 +37,21 @@ func (ome opMatchEngine) exec(next OpTree, c uintptr, p int) *opMatchContext {
 		case OpSuccess:
 			c := ctx.with(contextKey{'c', 0}, p)
 			ome.onSuccess(&c)
-			return &c
+			return true
 		case *OpStr:
 			if len(str)-p < op.minReq {
-				return nil
+				return false
 			}
 			for i := 0; i < len(op.str); i++ {
 				if str[p+i] != op.str[i] {
-					return nil
+					return false
 				}
 			}
 			next = op.follower
 			p += len(op.str)
 		case *OpAlt:
-			if r := ome.exec(op.follower, c, p); r != nil {
-				return r
+			if ome.exec(op.follower, c, p) {
+				return true
 			}
 			next = op.alt
 		case *OpRepeat:
@@ -61,33 +61,33 @@ func (ome opMatchEngine) exec(next OpTree, c uintptr, p int) *opMatchContext {
 				continue
 			}
 			ctx2 := ctx.with(op.key, p)
-			if r := ome.exec(op.follower, uintptr(unsafe.Pointer(&ctx2)), p); r != nil {
-				return r
+			if ome.exec(op.follower, uintptr(unsafe.Pointer(&ctx2)), p) {
+				return true
 			}
 			next = op.alt
 		case *OpClass:
 			if len(str)-p < op.minReq {
-				return nil
+				return false
 			}
 			r, size := utf8.DecodeRuneInString(str[p:])
 			if size == 0 || r == utf8.RuneError {
-				return nil
+				return false
 			}
 			if !op.cls.Contains(r) {
-				return nil
+				return false
 			}
 			next = op.follower
 			p += size
 		case *OpNotNewLine:
 			if len(str)-p < op.minReq {
-				return nil
+				return false
 			}
 			r, size := utf8.DecodeRuneInString(str[p:])
 			if size == 0 || r == utf8.RuneError {
-				return nil
+				return false
 			}
 			if r == '\n' {
-				return nil
+				return false
 			}
 			next = op.follower
 			p += size
@@ -100,18 +100,18 @@ func (ome opMatchEngine) exec(next OpTree, c uintptr, p int) *opMatchContext {
 		case *OpBackRef:
 			s, ok := ctx.GetCaptured(op.key)
 			if !ok || !strings.HasPrefix(str[p:], s) {
-				return nil
+				return false
 			}
 			next = op.follower
 			p += len(s)
 		case *OpAssertBegin:
 			if p != 0 {
-				return nil
+				return false
 			}
 			next = op.follower
 		case *OpAssertEnd:
 			if p != len(str) {
-				return nil
+				return false
 			}
 			next = op.follower
 		}
