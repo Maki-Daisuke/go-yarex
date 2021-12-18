57ef24b71653fe8a39407544ee1c33b0eb7996c5
Daisuke (yet another) Maki <maki.daisuke+gh@gmail.com>
2021-12-18 19:17:15 +0900

    Consolidate benchmark_test.go and sip_pattern_compile_test.go

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,16 +1,30 @@
-package yarex
+package yarex_test
 
 import (
+	"os"
 	"regexp"
 	"testing"
+
+	"github.com/Maki-Daisuke/go-yarex"
 )
 
+//go:generate cmd/yarexgen/yarexgen benchmark_test.go
+
 // This benchmark was barrowed from https://qiita.com/tj8000rpm/items/b92d7617883639a3e714
-var sipPattern = `^["]{0,1}([^"]*)["]{0,1}[ ]*<(sip|tel|sips):(([^@]*)@){0,1}([^>^:]*|\[[a-fA-F0-9:]*\]):{0,1}([0-9]*){0,1}>(;.*){0,1}$`
-var sipReAst, _ = parse(sipPattern)
-var sipReOpt = optimizeAst(sipReAst)
-var sipReOp = MustCompileOp(sipPattern)
+var sipPattern = `^["]{0,1}([^"]*)["]{0,1}[ ]*<(sip|tel|sips):(([^@]*)@){0,1}([^>^:]*|\[[a-fA-F0-9:]*\]):{0,1}([0-9]*){0,1}>(;.*){0,1}$` //yarexgen
 var sipReStd = regexp.MustCompile(sipPattern)
+var sipReAst, _ = yarex.Parse(sipPattern)
+var sipReOpt = yarex.OptimizeAst(sipReAst)
+var sipReOp = yarex.MustCompileOp(sipPattern)
+
+// Initialize sipReComp in TestMain, because it must be initialized after RegisterCompiledRegexp is called.
+var sipReComp *yarex.Regexp
+
+func TestMain(m *testing.M) {
+	sipReComp = yarex.MustCompile(sipPattern)
+	os.Exit(m.Run())
+}
+
 var testStrings = []string{"\"display_name\"<sip:0312341234@10.0.0.1:5060>;user=phone;hogehoge",
 	"<sip:0312341234@10.0.0.1>",
 	"\"display_name\"<sip:0312341234@10.0.0.1>",
@@ -29,7 +43,7 @@ func BenchmarkSipPattern_Standard(b *testing.B) {
 func BenchmarkSipPattern_Ast(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, s := range testStrings {
-			astMatch(sipReAst, s)
+			yarex.AstMatch(sipReAst, s)
 		}
 	}
 }
@@ -37,7 +51,7 @@ func BenchmarkSipPattern_Ast(b *testing.B) {
 func BenchmarkSipPattern_Optimized(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, s := range testStrings {
-			astMatch(sipReOpt, s)
+			yarex.AstMatch(sipReOpt, s)
 		}
 	}
 }
@@ -46,6 +60,17 @@ func BenchmarkSipPattern_Optree(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, s := range testStrings {
 			sipReOp.MatchString(s)
+		}
+	}
+}
+
+func BenchmarkSipPattern_Compiled(b *testing.B) {
+	if !yarex.IsCompiledMatcher(sipReComp) {
+		panic("Not compiled!!!!!")
+	}
+	for i := 0; i < b.N; i++ {
+		for _, s := range testStrings {
+			sipReComp.MatchString(s)
 		}
 	}
 }
